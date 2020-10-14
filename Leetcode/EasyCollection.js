a38a4130dce38c5c9174aff3b2ab556488f09d64
AndrewCWheeler <achristopherwheeler@gmail.com>
2020-10-14 11:13:10 -0700

    Added Valid Sudoku

--- Leetcode/EasyCollection.js
+++ Leetcode/EasyCollection.js
@@ -160,16 +160,16 @@ const input1 = [1, 2, 3, 1];
 const input2 = [1, 2, 3, 4];
 
 var containsDuplicate = function (nums) {
-  // for (let i = 0; i < nums.length - 1; i++) {
-  //   for (let j = i + 1; j <= nums.length - 1; j++) {
-  //     console.log(`This is nums[i]: ${nums[i]}`);
-  //     console.log(`This is nums[j]: ${nums[j]}`);
-  //     if (nums[j] === nums[i]) {
-  //       return true;
-  //     }
-  //   }
-  // }
-  // return false;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      console.log(`This is nums[i]: ${nums[i]}`);
+      console.log(`This is nums[j]: ${nums[j]}`);
+      if (nums[j] === nums[i]) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 // console.log(containsDuplicate(input1));
@@ -300,9 +300,95 @@ var plusOne = function (digits) {
     return digits;
   }
 };
-console.log(plusOne(digits));
+// console.log(plusOne(digits));
 
 // var plusOne2 = function (digits) {
 //   return (BigInt(digits.join('')) + 1n).toString().split('');
 // };
 // console.log(plusOne2(digits));
+
+// Valid Sudoku
+
+// Solution
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+// Example 1:
+
+// Input: board =
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: true
+
+// Example 2:
+
+const board = [
+  ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+  ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+  ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+  ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+  ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+  ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+  ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+  ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+  ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+];
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+// Constraints:
+
+// board.length == 9
+// board[i].length == 9
+// board[i][j] is a digit or '.'.
+
+const isValidSudoku = (board) => {
+  for (let i = 0; i < board.length; i++) {
+    let rowMap = {}; 
+    let colMap = {}; 
+    let gridMap = {}; 
+    for (let j=0; j<board.length; j++){
+      // row validation
+      if (board[i][j]!=='.'&& rowMap[board[i][j]]){
+        return false; 
+      } else {
+        rowMap[board[i][j]] = true; 
+      }
+      // col validation
+      if (board[j][i]!='.' && colMap[board[j][i]]){
+        return false; 
+      } else {
+        colMap[board[j][i]] = true; 
+      }
+      // 3*3 grid validation
+      let rowIndex = Math.floor(3*(i/3));
+      let colIndex = Math.floor(3*(i%3));
+      console.log(rowIndex);
+      console.log(colIndex); 
+
+      if (board[rowIndex+Math.floor(j/3)][colIndex+Math.floor(j%3)] !='.' && gridMap[board[rowIndex+Math.floor(j/3)][colIndex+Math.floor(j%3)]]){
+        return false;
+      } else {
+        gridMap[board[rowIndex+Math.floor(j/3)][colIndex+Math.floor(j%3)]] = true; 
+      }
+
+      }
+    }
+    return true; 
+  };
+
+console.log(isValidSudoku(board));
