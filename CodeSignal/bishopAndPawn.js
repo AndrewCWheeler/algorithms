83cfa6b3b5df244ae38ba8947bd4377458b038c6
AndrewCWheeler <achristopherwheeler@gmail.com>
2021-08-30 16:06:42 -0700

    updated bishopAndPawn to one line

--- CodeSignal/bishopAndPawn.js
+++ CodeSignal/bishopAndPawn.js
@@ -1,14 +1,5 @@
 const bishopAndPawn = (bishop, pawn) => {
-    let bishopCode = bishop[0].charCodeAt(0);
-    let pawnCode = pawn[0].charCodeAt(0);
-
-    let bishopNum = Number(bishop[1]);
-    let pawnNum = Number(pawn[1]);
-
-    let codeDiff = Math.abs(bishopCode - pawnCode);
-    let numDiff = Math.abs(bishopNum - pawnNum);
-
-    return codeDiff === numDiff;
+    return Math.abs(bishop[0].charCodeAt(0)) - pawn[0].charCodeAt(0) === Math.abs(Number(bishop[1]) - Number(pawn[1]));
 }
 
 console.log(bishopAndPawn("g1", "e3"));
