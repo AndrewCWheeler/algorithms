13972d91c381b721595fee6f587b5e5026f836f9
AndrewCWheeler <achristopherwheeler@gmail.com>
2021-09-27 23:24:06 -0700

    Added PrintArray, FindMax, and GetAverage in .NET 6.0

--- CSharpConsole/Program.cs
+++ CSharpConsole/Program.cs
@@ -40,11 +40,69 @@ namespace CSharpConsole
             }
         }
 
+        // Iterating through an Array
+        // Write a function that would iterate through each item of the given integer array and print each value to the console.
+        public static void PrintArray(int[] arr)
+        {
+            Console.Write("{ ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == arr.Length - 1)
+                {
+                    Console.Write("{0}", arr[i]);
+                }
+                else
+                {
+                    Console.Write("{0}, ", arr[i]);
+                }
+            }
+            Console.WriteLine(" }");
+        }
+        // Find Max
+        // Write a function that takes an integer array and prints and returns the maximum value in the array. 
+        // Your program should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]), copy
+        // or even a mix of positive numbers, negative numbers and zero.
+
+        public static int FindMax(int [] arr)
+        {
+            int MaxVal = arr[0];
+            for (int i = 0; i < arr.Length; i++) 
+            { 
+                if (arr[i] > MaxVal)
+                {
+                    MaxVal = arr[i];
+                }
+            }
+            Console.WriteLine(MaxVal);
+            return MaxVal;
+        }
+
+        // Get Average
+        // Write a function that takes an integer array and prints the AVERAGE of the values in the array.copy
+        // For example, with an array [2, 10, 3], your program should write 5 to the console.
+
+        public static void GetAverage(int[] arr)
+        {
+            int Avg = 0;
+            int Sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Sum += arr[i];
+            }
+            Avg = Sum / arr.Length;
+            Console.WriteLine(Avg);
+        }
+
         public static void Main(string[] args)
         {
+            int[] NumArray = new int[] {3, 6, 9, 12, -9, 9, 15};
             PrintNums(255);
             PrintOdds(255);
             PrintNumsAndSum(255);
+            PrintArray(NumArray);
+            FindMax(NumArray);
+            GetAverage(NumArray);
+            
         }
     }
 }
