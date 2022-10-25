8f48ab2716659d10235ab4b4c42a89f122c47504
Andrew <andrew@Andrews-iMac.local>
2022-10-24 18:38:56 -0700

    fixed pop, added shift

--- Udemy/LinkedLists/DLList.js
+++ Udemy/LinkedLists/DLList.js
@@ -13,6 +13,16 @@ class DLList {
     this.length = 0;
   }
 
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    return arr;
+  }
+
   push(val) {
     var newNode = new Node(val);
     if (!this.head) {
@@ -34,21 +44,38 @@ class DLList {
       this.head === null;
       this.tail === null;
     } else {
-      this.tail === oldTail.prev;
+      this.tail = oldTail.prev;
       this.tail.next = null;
       oldTail.prev = null;
     }
     this.length--;
     return oldTail;
   }
+  shift() {
+    if (!this.head) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      oldHead.next = null;
+      this.head.prev = null;
+    }
+    this.length--;
+    return oldHead;
+  }
 }
 
 const first = new DLList();
 
 first.push(1);
-console.log(first);
 first.push(2);
-console.log(first);
-console.log(first.push(3));
+first.push(3);
+console.log(first.print());
+console.log("first.pop():");
 console.log(first.pop());
-console.log(first);
+console.log(first.print());
+console.log("first.shift():");
+console.log(first.shift());
+console.log(first.print());
