b72b3d2f8c051a1dc2e0ed69c68e0e2f9a611fdc
Andrew <andrew@Andrews-iMac.local>
2022-11-30 21:19:54 -0800

    fixed BST insert() + BFS()

--- Udemy/Trees/binarySearchTree.js
+++ Udemy/Trees/binarySearchTree.js
@@ -16,22 +16,25 @@ class BST {
     if (this.root === null) {
       this.root = newNode;
       return this;
-    }
-    var current = this.root;
-    while (true) {
-      if (val === current.val) return undefined;
-      if (val < current.val) {
-        if (current.left === null) {
-          current.left = newNode;
-          return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
         }
-        current = current.left;
-      } else if (val > current.val) {
-        if (current.right === null) {
-          current.right === newNode;
-          return this;
-        }
-        current = current.right;
       }
     }
   }
@@ -50,17 +53,30 @@ class BST {
     }
     return false;
   }
+  BFS() {
+    var node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
 
 var tree = new BST();
 
-tree.insert(6);
-tree.insert(5);
-tree.insert(4);
-tree.insert(3);
 tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree.contains(3));
-console.log(tree.contains(2));
-console.log(tree.contains(11));
-console.log(tree.contains(5));
+console.log(tree);
+
+console.log(tree.BFS());
